a31f8512b3a2601110ea78d4d14ac2baa64d6412
oliver.reese@axelspringer.com <oliver.reese@axelspringer.de>
2021-04-15 15:34:43 +0200

    added 2nd test

---
+++ tests/AnotherTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AnotherTest extends TestCase
+{
+    public function testDummy() {
+        $this->assertTrue(true);
+    }
+}
