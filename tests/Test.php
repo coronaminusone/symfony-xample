9a3c82825d702f9904deefaa9bd544ad04091c29
oliver.reese@axelspringer.com <oliver.reese@axelspringer.de>
2021-04-15 15:31:58 +0200

    added test

---
+++ tests/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use DemoTest;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testDummy() {
+        $this->assertTrue(true);
+    }
+}
